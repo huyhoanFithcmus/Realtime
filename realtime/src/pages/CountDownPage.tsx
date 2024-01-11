d2801515fb8a8be4e0cc9f6cbeddb13bd4518826
huyhoanfithcmus <huyhoan652002@gmail.com>
2024-01-11 22:30:18 +0700

    responsive

--- realtime/src/pages/CountDownPage.tsx
+++ realtime/src/pages/CountDownPage.tsx
@@ -2,6 +2,7 @@ import Countdown from 'react-countdown'
 
 export default function CountDownPage() {
   const DeadlineTime = new Date('2024-07-30T00:00:00')
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const renderer = ({ days, hours, minutes, seconds, completed }: any) => {
     if (completed) {
@@ -16,13 +17,13 @@ export default function CountDownPage() {
       // Render a countdown
       return (
         <>
-          <div className='w-full h-screen bg-gradient-to-bl from-teal-400 to-blue-500 flex flex-col justify-center items-center text-white'>
-            <h1 className='text-5xl p-4'>
-              <b>ROBOCUS 2024</b> is coming!
+          <div className='w-full h-screen bg-gradient-to-bl from-teal-400 to-blue-500 flex flex-col justify-center items-center text-white overflow-hidden'>
+            <h1 className='text-5xl p-4 text-center'>
+              <b>ROBOCUS 2024</b>
             </h1>
             <h2 className='text-2xl'>Will be held after</h2>
 
-            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 mt-10 lg:mt-20'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 mt-10 lg:mt-20 overflow-x-auto'>
               <div className='bg-transparent border text-center'>
                 <p className='text-5xl px-10 py-5'>{days}</p>
                 <hr />
@@ -40,6 +41,7 @@ export default function CountDownPage() {
                 <hr />
                 <p className='px-10 py-5'>mins</p>
               </div>
+
               <div className='bg-transparent border text-center'>
                 <p className='text-5xl px-10 py-5'>{seconds}</p>
                 <hr />
@@ -51,9 +53,10 @@ export default function CountDownPage() {
       )
     }
   }
+
   return (
     <>
-      <div>
+      <div className='overflow-hidden'>
         <Countdown date={DeadlineTime} renderer={renderer} />
       </div>
     </>
